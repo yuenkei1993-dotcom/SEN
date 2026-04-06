6135be1f03671cc5075b4b01b4370ed607cff5cc
yuenkei1993-dotcom <yuenkei1993@gmail.com>
2026-04-06 18:17:31 +0800

    fix(constants): Update student sentences and image URLs
    Corrected incorrect image URLs and updated student sentences for individual students to accurately reflect their names.

--- src/constants.ts
+++ src/constants.ts
@@ -12,11 +12,11 @@ export const INITIAL_TASKS: Task[] = [
   { id: 't1-2', studentId: '1', correctImageUrl: 'https://i.postimg.cc/Hx3SK5cb/Get-Img.jpg', incorrectImageUrl: 'https://i.postimg.cc/YqF6LYrz/shutterstock-710865547.jpg', sentence: '吃棉花糖', happySentence: '吃棉花糖' },
   { id: 't1-3', studentId: '1', correctImageUrl: 'https://i.postimg.cc/wvscpGBZ/images.jpg', incorrectImageUrl: 'https://i.postimg.cc/FsnRyspR/images-(1).jpg', sentence: '好開心', happySentence: '好開心' },
   // 周海濤
-  { id: 't2-1', studentId: '2', correctImageUrl: 'https://i.postimg.cc/rpX59mm2/zhou-hai-tao-(2).png', incorrectImageUrl: 'https://i.postimg.cc/FzZTMSYW/li-xu-shan.png', sentence: '高毓軒', happySentence: '高毓軒' },
+  { id: 't2-1', studentId: '2', correctImageUrl: 'https://i.postimg.cc/rpX59mm2/zhou-hai-tao-(2).png', incorrectImageUrl: 'https://i.postimg.cc/JHs3JGcZ/li-xu-shan.png', sentence: '周海濤', happySentence: '周海濤' },
   { id: 't2-2', studentId: '2', correctImageUrl: 'https://i.postimg.cc/Hx3SK5cb/Get-Img.jpg', incorrectImageUrl: 'https://i.postimg.cc/YqF6LYrz/shutterstock-710865547.jpg', sentence: '吃棉花糖', happySentence: '吃棉花糖' },
   { id: 't2-3', studentId: '2', correctImageUrl: 'https://i.postimg.cc/wvscpGBZ/images.jpg', incorrectImageUrl: 'https://i.postimg.cc/FsnRyspR/images-(1).jpg', sentence: '好開心', happySentence: '好開心' },
   // 楊翹丹
-  { id: 't3-1', studentId: '3', correctImageUrl: 'https://i.postimg.cc/1tRnWN1Y/yang-qiao-dan.png', incorrectImageUrl: 'https://i.postimg.cc/FzZTMSYW/li-xu-shan.png', sentence: '高毓軒', happySentence: '高毓軒' },
+  { id: 't3-1', studentId: '3', correctImageUrl: 'https://i.postimg.cc/1tRnWN1Y/yang-qiao-dan.png', incorrectImageUrl: 'https://i.postimg.cc/JHs3JGcZ/li-xu-shan.png', sentence: '楊翹丹', happySentence: '楊翹丹' },
   { id: 't3-2', studentId: '3', correctImageUrl: 'https://i.postimg.cc/Hx3SK5cb/Get-Img.jpg', incorrectImageUrl: 'https://i.postimg.cc/YqF6LYrz/shutterstock-710865547.jpg', sentence: '吃棉花糖', happySentence: '吃棉花糖' },
   { id: 't3-3', studentId: '3', correctImageUrl: 'https://i.postimg.cc/wvscpGBZ/images.jpg', incorrectImageUrl: 'https://i.postimg.cc/FsnRyspR/images-(1).jpg', sentence: '好開心', happySentence: '好開心' },
 ];
